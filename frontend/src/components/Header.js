84408c0b84e9ceaef6e12372e7cbd792748b4f16
Amila-Janaka <janakaamila28@gmail.com>
2024-11-22 16:10:28 +0530

    few changes with btns and colors

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -6,12 +6,13 @@ import { CiSearch } from "react-icons/ci";
 import { IoMdCart } from "react-icons/io";
 import { Link } from "react-router-dom";
 import DropdownMenu from "./DropdownMenu";
+import "./Components CSS/Header.css";
 
 
 
 const Header = () => {
     return (
-        <header className="h-16 shadow-md bg-white">
+        <header className="header h-16 shadow-md bg-gray-700">
             <div className="h-full container-fluid mx-auto flex items-center px-10 justify-between">
                 <div className="">
                     <Link to={"/"}>
@@ -25,9 +26,9 @@ const Header = () => {
 
                 </div>
 
-                <div className="hidden lg:flex items-center w-full justify-between max-w-sm border rounded-full focus-within:shadow-md pl-2">
-                    <input type="text" placeholder="Search Products Here..." className="w-full outline-none" />
-                    <div className="text-lg min-w-[50px] h-8 bg-black-600 flex items-center justify-center rounded-r-full">
+                <div className="hidden lg:flex items-center w-full justify-between max-w-sm border rounded-full focus-within:shadow-lg pl-2 ">
+                    <input type="text" placeholder="Search Products Here..." className="w-full outline-none text-white bg-transparent" />
+                    <div className="text-lg min-w-[50px] h-8 bg-gray-700 flex items-center justify-center rounded-r-full text-white">
                         <CiSearch />
                     </div>
                 </div>
@@ -41,16 +42,16 @@ const Header = () => {
                     </div> */}
 
 
-                    <div className="text-l cursor-pointer relative">
+                    <div className="text-l cursor-pointer relative text-white hover:text-yellow-500">
                         <span>Explore</span>
                     </div>
 
-                    <div className="text-l cursor-pointer relative">
+                    <div className="text-l cursor-pointer relative text-white hover:text-yellow-500">
                         <span>For Business</span>
                     </div>
 
-                    <div className="text-2xl cursor-pointer relative">
-                        <span className="text-black-500 "><DropdownMenu /></span>
+                    <div className="text-2xl cursor-pointer relative ">
+                        <span className="text-white "><DropdownMenu /></span>
 
                         {/* <div className="px-3 py-1 rounded-full border text-black font-bold hover:bg-blue-100">
                             <Link to={"/login"}>Login</Link>
@@ -61,9 +62,9 @@ const Header = () => {
 
                     </div>
 
-                    <div className="text-2xl cursor-pointer relative">
-                        <span className="text-black-500 "><IoMdCart /></span>
-                        <div className="bg-black-600 text-black w-4 h-4 rounded-full p-1 flex items-center justify-center absolute -top-2 -right-3 ">
+                    <div className="text-2xl cursor-pointer relative ">
+                        <span className="text-white "><IoMdCart /></span>
+                        <div className="bg-black-600 text-white w-4 h-4 rounded-full p-1 flex items-center justify-center absolute -top-2 -right-3 ">
                             <p className="text-sm">0</p>
                         </div>
                     </div>
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -70,13 +70,13 @@ const Login = () => {
             <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
                 <div class="sm:mx-auto sm:w-full sm:max-w-sm">
                     <img src={LoginIcon} alt="login-icon" className="w-40 h-40 mx-auto" />
-                    <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Sign in to your account</h2>
+                    <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-white">Sign in to your account</h2>
                 </div>
 
                 <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
                     <form class="space-y-6" action="#" method="POST">
                         <div>
-                            <label for="email" class="block text-sm/6 font-medium text-gray-900">Email address</label>
+                            <label for="email" class="block text-sm/6 font-medium text-white">Email address</label>
                             <div class="mt-2">
                                 <input id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6" />
                             </div>
@@ -84,9 +84,9 @@ const Login = () => {
 
                         <div>
                             <div class="flex items-center justify-between">
-                                <label for="password" class="block text-sm/6 font-medium text-gray-900">Password</label>
+                                <label for="password" class="block text-sm/6 font-medium text-white">Password</label>
                                 <div class="text-sm">
-                                    <Link to={"/forgotPassword"} class="font-semibold text-indigo-600 hover:text-indigo-500">
+                                    <Link to={"/forgotPassword"} class="font-semibold text-yellow-500 hover:text-yellow-400">
                                         Forgot password?
                                     </Link>
 
@@ -94,7 +94,7 @@ const Login = () => {
                             </div>
                             <div class="mt-2 flex items-center">
                                 <input id="password" name="password" type={showPassword ? "text" : "password"} autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6" />
-                                <span className="text-xl px-2 cursor-pointer" onClick={() =>
+                                <span className="text-xl px-2 cursor-pointer text-white" onClick={() =>
                                     setShowPassword((preve) => !preve)
                                 }>
                                     {
@@ -112,13 +112,13 @@ const Login = () => {
                         </div>
 
                         <div>
-                            <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+                            <button type="submit" class="flex w-full justify-center rounded-md bg-yellow-500 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-yellow-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
                         </div>
                     </form>
 
                     <p class="mt-10 text-center text-sm/6 text-gray-500">
                         Not a member?
-                        <Link to={"/signIn"} class="font-semibold text-indigo-600 hover:text-indigo-500"> Create Account</Link>
+                        <Link to={"/signIn"} class="font-semibold text-yellow-500 hover:text-yellow-400"> Create Account</Link>
                     </p>
                 </div>
             </div>
